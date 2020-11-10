ed2aca233ecac082f2d12e67ba3063628ccd0f47
Ian Hakeman <hakeman@chapman.edu>
2020-11-09 17:21:42 -0800

    Adds username.sh: functional and without shellcheck errors

--- username.sh
+++ username.sh
@@ -6,11 +6,11 @@ echo "Use lower case characters, digits, and underscores only"
 echo "It must start with a lower case character"
 echo "It must contain at least 3 but not more than 12 characters"
 
-read USERNAME
-while echo "$USERNAME" | egrep -v "^[a-z]{1}[a-z0-9_]{2,11}$" > /dev/null 2>&1
+read -r USERNAME
+while echo "$USERNAME" | grep -Ev "^[a-z]{1}[a-z0-9_]{2,11}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a username: "
-	read USERNAME
+	read -r USERNAME
 done
 echo "Thank you"
